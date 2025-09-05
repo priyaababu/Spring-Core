983bb313c96512aedb4fb656fc51d5d440b8f353
priyaababu <priyaababu12@gmail.com>
2025-09-05 21:34:03 +0530

    Core2

---
+++ spring-core-2/src/main/java/com/tnsif/spring_core_2/App.java
@@ -0,0 +1,17 @@
+package com.tnsif.spring_core_2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class App {
+  public static void main(String[] args) {
+    
+	  //loads the config file
+	  ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+  
+	  //getBean
+  College college = context.getBean("Colege" ,College.class);
+  
+   college.showCollegeDetails(); 
+  }
+ }
+
